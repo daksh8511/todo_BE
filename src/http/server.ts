50e62d3085f1dfcc65cbd2700f1546b5a312907c
Daksh03 <daxsathwara102@gmail.com>
2026-04-24 14:21:01 +0530

    origins add

--- src/http/server.ts
+++ src/http/server.ts
@@ -8,7 +8,11 @@ import cors from '@fastify/cors'
 const app = fastify({ logger: true });
 
 await app.register(cors, {
-  origin: "http://localhost:5173",
+  origin: [
+    "https://todo-blush-mu.vercel.app",
+    "http://localhost:5173",
+    "http://127.0.0.1:5173"
+  ],
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
 });
 
